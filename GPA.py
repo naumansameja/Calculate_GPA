6fbc6fde6399e7a595675aa1ceb7b7f1c5b7e5d7
naumansameja <96863956+naumansameja@users.noreply.github.com>
2022-08-26 19:05:37 +0500

    Add files via upload

---
+++ GPA.py
@@ -0,0 +1,21 @@
+class GPA:
+    def __init__(self, d):
+        self.dictionary = d
+        self.grades = {"A": 4, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "D": 1.0, "F": 0.0}
+
+    def get_gpa(self):
+        total_credit = 0
+        quality_points = 0
+        for i in self.dictionary:
+            total_credit += self.dictionary[i]
+            print("Enter the grade in", end=" ")
+            grade = self.grades[grade]
+            sum = self.subject_gpa(grade) * self.dictionary[i]
+            quality_points += sum
+            sum = 0
+        return quality_points / total_credit
+
+
+sem1 = GPA({"PF": 3, "PFL": 1, "QT": 0.5, "ICT": 2, "ICTL": 1, "Eng": 3, "PST": 2})
+gpa = sem1.get_gpa()
+print(gpa)
